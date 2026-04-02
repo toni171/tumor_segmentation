c5f878e90cb235bb0daa86869e39e0c03d98b515
toni171 <dileo01antonio@gmail.com>
2026-04-02 11:52:05 +0200

    Added overall results

--- main.m
+++ main.m
@@ -15,3 +15,26 @@ m = m.calculateEvalMetrics();
 fprintf("Accuracy %f\n",m.eval_metrics.Precision);
 i = i+1;
 end
+results_path = "results.json";
+results = readstruct(results_path);
+img_names = fieldnames(results);
+metric_names = fieldnames(results.(img_names{1}));
+metrics = struct();
+average = struct();
+for j=1:numel(img_names)
+    for i=1:numel(metric_names)
+        if j == 1
+            metrics.(metric_names{i}) = [];
+        end
+        metrics.(metric_names{i}) = ...
+            [metrics.(metric_names{i}) (results.(img_names{j}).(metric_names{i}))];
+        if j == numel(img_names)
+            average.(metric_names{i}) = mean(metrics.(metric_names{i}));
+        end
+    end
+end
+results.overall_results = average;
+content = jsonencode(results,"PrettyPrint",true);
+fid = fopen(results_path,'w');
+fprintf(fid,"%s\n",content);
+fclose(fid);

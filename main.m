5733aebaf1c33015b881350e3de8f9e0caee63fa
toni171 <dileo01antonio@gmail.com>
2026-04-09 13:33:45 +0200

    Fixed aggregation metrics

--- main.m
+++ main.m
@@ -2,7 +2,7 @@ close all;
 clear;
 clc;
 i = 1;
-while i<=11
+while i<=1
 m = SegmenterModel('config.json','results.json');
 m = m.setImage(i);
 m = m.preprocessImage();
@@ -10,7 +10,7 @@ m = m.levelSetEvolution();
 m = m.extractRegionProps();
 m = m.tumorIdentification();
 m = m.calculateEvalMetrics();
-%m.showEvolution(10,0.2);
+m.showEvolution(10,0.2);
 %m.showSegmentation();
 fprintf("Accuracy %f\n",m.eval_metrics.Precision);
 i = i+1;
